e5cbcf85e3d0622a568a88d9a879d8a01a580234
Sophia Breithaupt <sophia.breithaupt@schiller-og.de>
2022-09-27 09:14:27 +0200

    erstercommit

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+
+import java.beans.PropertyVetoException;
+public class Test {
+    public static void main(String args[]){
+        Vertex v1 = new Vertex(4, 3);
+        Vertex v2 = new Vertex(34, 2);
+        Vertex v4 = new Vertex(4, 3);
+        Vertex vv = new Vertex(0, 0);
+        /*
+        System.out.println(v1);
+        System.out.println(v1.distance());
+        System.out.println(v2);
+        System.out.println(v2.distance());
+        System.out.println(vv);
+        System.out.println(vv.distance());
+        */
+        v1.skalarMultMod(3);
+        System.out.println(v1);
+
+
+        System.out.println(v1.equals(v2));
+        System.out.println(v1.equals(v4));
+
+        
+
+        
+
+    }
+}
