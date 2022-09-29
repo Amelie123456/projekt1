c11601f156edc80ffe9f477aa3c648b6f9758b36
Sophia Breithaupt <sophia.breithaupt@schiller-og.de>
2022-09-29 15:29:37 +0200

    29.9.2022

--- src/GeometricObject.java
+++ src/GeometricObject.java
@@ -45,4 +45,48 @@ public class GeometricObject {
     public String toString(){
         return "width=" +width+"height=" +height+"pos="+pos;
     }
+
+    public double circumference(){
+        return (height+width)*2;
+    }
+
+    public double area(){
+        return (height*width);
+    }
+
+    public boolean contains(Vertex v){
+        return v.x >= this.pos.x && this.pos.x+width >= v.x &&
+               v.y >= this.pos.y && this.pos.y+height >= v.y;
+    }
+
+    public boolean isLargerThan(GeometricObject g){
+        return g.area()<this.area();
+    }
+
+    public void moveTo(Vertex v){
+        this.pos= v;
+    }
+
+    public void moveTo(double x, double y){
+        moveTo(new Vertex(x,y));
+    }
+
+    public void move(Vertex v){
+        moveTo(this.pos.add(v));
+    }
+
+    public boolean equals(Object thatObject){
+        if (thatObject instanceof GeometricObject){
+            GeometricObject that = (GeometricObject) thatObject;
+            return this.height == that.height && this.width == that.width && this.pos.equals(that.pos);
+        }
+        return false;
+    }
+
+    public boolean contains(GeometricObject that){
+        Vertex newV =new Vertex(this.pos.x - that.width, this.pos.y - that.height);
+        GeometricObject big = new GeometricObject(newV, this.width+that.width, this.height+that.height);
+        return big.contains (that.pos);
+        
+    }
 }
