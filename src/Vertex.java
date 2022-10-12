163dd040c76b90d1adf9e9b3cb99d73483d36743
Sophia Breithaupt <sophia.Breithaupt@schiller-og.de>
2022-10-12 09:30:36 +0200

    12.10.2022

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,61 +1,60 @@
 public class Vertex {
 
-      public double x;
-      public double y;
+   public double x;
+   public double y;
 
-      public Vertex(double x, double y){
-          this.x=x;
-          this.y=y;
-      }
-      public String toString(){return "("+x+","+y+")";}
-
-      public Double distance(){return Math.sqrt(x*x + y*y);}
-
-      public Vertex skalarMult(double s){return new Vertex(x*s, y*s);}
-
-      public void skalarMultMod(double s){this.x=s*x; this.y=s*y;}
-
-      public void addMod (Vertex v2){
-         this.x=x+v2.x;
-      }
-
-      public Vertex add(Vertex v2){
-         return new Vertex(x+v2.x, y+v2.y);
-      }
-
-      public double getX (){return x;}
-      public double getY (){return y;}
-
-      public void setX (double x){
-         this.x=x;
-      }
-
-      public void setY (double y){
-         this.y=y;
-      }
-
-      public boolean equals(Object thatObject){
-         if (thatObject instanceof Vertex){
-            Vertex that = (Vertex)thatObject;
-            return this.x==that.x && this.y==that.y;
-         }
-         return false;
-      }
-
-      public Vertex sub(Vertex that){
-         return new Vertex (x-that.x, y-that.y);
-      }
-
-      public Double distance(Vertex that){
-         double a= that.x-this.x;
-         double b= that.y-this.y;
-         return Math.sqrt(a*a+b*b);
-      }
-      public void normalize(){
-         double factor = this.distance;
-
-         this.x=factor/this.distance;
-         this.y=factor/this.distance;
-      }
-   
+   public Vertex(double x, double y) {
+       this.x = x;
+       this.y = y;
    }
+
+   public String toString() {
+       return "(" + x + "," + y + ")";
+   }
+
+   public double length() {
+       return Math.sqrt(x * x + y * y);
+   }
+
+   public void skalarMod(double s) {
+       this.x = x * s;
+       this.y = y * s;
+   }
+
+   public Vertex skalarMultMod(double s) {
+       return new Vertex(x * s, y * s);
+   }
+
+   public Vertex add(Vertex v2) {
+       return new Vertex(x + v2.x, y + v2.y);
+   }
+
+   public void addMod(Vertex v2) {
+       this.x = x + v2.x;
+   }
+
+   public double getX() {
+       return x;
+   }
+
+   public double getY() {
+       return y;
+   }
+
+   public void setX(double x) {
+       this.x = x;
+   }
+
+   public void setY(double y) {
+       this.y = y;
+   }
+
+   public boolean equals(Object thatObject) {
+       if (thatObject instanceof Vertex) {
+           Vertex that = (Vertex) thatObject;
+           return this.x == that.x && this.y == that.y;
+       }
+       return false;
+   }
+
+}
---
+++ src/util/SizedPanel.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import util.ShowInFrame;
+
+import java.awt.Dimension;
+public class SizedPanel extends JPanel {
+
+    int preferredwidth=1500;
+    int preferredheight=1000;
+
+    public SizedPanel(int w, int h){
+        preferredwidth=w;
+        preferredheight=h;
+    }
+    public SizedPanel(){}
+    
+    @Override
+    public Dimension getPreferredSize(){
+        return new Dimension(preferredwidth,preferredheight);
+    }
+
+    public static void main(String[] args){
+        ShowInFrame.show(new SizedPanel());
+    }
+    }
+
